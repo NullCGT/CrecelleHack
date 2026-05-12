aa9bbfd7e2243b2d7241e2bef0dba6be4f70414e
Kestrel <kestrelg@kestrelscry.com>
2026-05-12 10:03:22 -0500

    Improve terrain boost grammar in enlightenment menu.

--- src/insight.c
+++ src/insight.c
@@ -782,10 +782,10 @@ background_enlightenment(int unused_mode UNUSED, int final)
     }
 
     /* terrain boosts */
-    Sprintf(buf, " Your %s depends on terrain:", u.ualign.type == A_CHAOTIC ? "speed"
+    Sprintf(buf, "%s changes depending on terrain:", u.ualign.type == A_CHAOTIC ? "speed"
                                                 : u.ualign.type == A_LAWFUL ? "AC"
                                                     : "energy regeneration");
-    enlght_out(buf);
+    you_have(buf, "");
     if (Race_if(PM_DWARF)) {
         Snprintf(buf, sizeof(buf), "a preference for bare earth");
         you_have(buf, "");
